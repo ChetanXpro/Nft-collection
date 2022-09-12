65f9f9451dc74b410b75eab7dc7602ae3f51e56b
Chetan baliyan <107798155+ChetanXpro@users.noreply.github.com>
2022-09-12 13:39:31 +0530

    Update index.js

--- Contracts/constants/index.js
+++ Contracts/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xe4A6AD947Acdf509C426a5c90C64bAFE99Dd3B7c";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
