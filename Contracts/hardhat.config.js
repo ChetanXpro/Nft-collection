a821d1d5934d40c2d14fc5c0819542e1313319e1
Chetan baliyan <107798155+ChetanXpro@users.noreply.github.com>
2022-09-12 13:54:06 +0530

    Update hardhat.config.js

--- Contracts/hardhat.config.js
+++ Contracts/hardhat.config.js
@@ -6,7 +6,7 @@ require("dotenv").config({ path: ".env" });
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    rinkeby: {
+    goerli: {
       url: process.env.ALCHEMY_API_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
