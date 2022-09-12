a4b316d159eacbd1ed46ef59140047c521c4439a
Chetan baliyan <107798155+ChetanXpro@users.noreply.github.com>
2022-09-12 18:17:07 +0530

    Update index.js

--- Contracts/constants/index.js
+++ Contracts/constants/index.js
@@ -1,5 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0xe4A6AD947Acdf509C426a5c90C64bAFE99Dd3B7c";
+const WHITELIST_CONTRACT_ADDRESS = "0x8872d7e7630ABda440A1e1E237D4854f44ef41Eb" 
+//       "0xe4A6AD947Acdf509C426a5c90C64bAFE99Dd3B7c";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-mslusltnp-chetanxpro.vercel.app/api/";
 
